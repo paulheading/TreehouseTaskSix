96a32bd6ed42706e895487946e887e618b88d015
Paul Heading <paul.heading@barbican.org.uk>
2020-11-13 10:04:23 +0000

    details updated

---
+++ README.md
@@ -0,0 +1,5 @@
+# Treehouse Task Six : Build an API Scraper with Node JS
+
+To run the project, open terminal from the root folder and type 'node scraper.js'
+
+![screengrab](https://repository-images.githubusercontent.com/142123578/3d60ac80-2596-11eb-9d04-297f87939db6)
--- scraper.js
+++ scraper.js
@@ -1,4 +1,3 @@
-
 // simple way to make http calls
 const request = require("request");
 // jquery style functionality
@@ -10,17 +9,21 @@ const moment = require("moment");
 // file + folder creation
 const fs = require("fs");
 
-const errorFunc = function(error) {
-  console.log("The scraper had an issue with " + url + ". Either the site is down or your connection is bad.");
+function printError(error) {
+  console.log(
+    "The scraper had an issue with " +
+      url +
+      ". Either the site is down or your connection is bad."
+  );
   const errorDate = new Date();
   const errorLog = `[${errorDate}] ${error.message}`;
 
   // write the error log to file
-  fs.appendFile("scraper-error.log", errorLog, function(err) {
+  fs.appendFile("scraper-error.log", errorLog, (err) => {
     if (err) throw err;
     console.log("There was an error. It has been logged to scraper-error.log");
   });
-};
+}
 
 const url = "http://shirts4mike.com";
 const totalShirts = new Array();
@@ -28,18 +31,17 @@ const shirtsToScrape = [];
 const linksSeen = [];
 
 // promise structure for scrape
-const requestPromise = function(url) {
-  return new Promise(function(resolve, reject) {
-      request(url, function(error, response, html) {
+function requestPromise(url) {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, html) => {
+      // if there's an error, send error details
+      if (error) return reject(error);
 
-          // if there's an error, send error details
-          if(error) return reject(error);
-
-          // if not, send html details
-          if(!error && response.statusCode == 200){
-              return resolve(html);
-          }
-      });
+      // if not, send html details
+      if (!error && response.statusCode == 200) {
+        return resolve(html);
+      }
+    });
   });
 }
 
@@ -48,41 +50,41 @@ firstScrape(url)
   .then(secondScrape)
   .then(thirdScrape)
   .then(writeToFile)
-  .catch(function(error) {
+  .catch((error) => {
     console.log(error);
-});
+  });
 
 function firstScrape(url) {
   // what to do with first html details
   return requestPromise(url)
-  .then(function(html) {
-    const $ = cheerio.load(html);
-    // find all hrefs with the word shirt
-    $("a[href*='shirt']").each(function() {
-      const href = $(this).attr("href");
-      const fullPath = `${url}/${href}`;
-      // add the full path of all links found on the homepage
-      if (linksSeen.indexOf(fullPath) === -1) {
-        linksSeen.push(fullPath);
-      }
+    .then((html) => {
+      const $ = cheerio.load(html);
+      // find all hrefs with the word shirt
+      $("a[href*='shirt']").each(function () {
+        const href = $(this).attr("href");
+        const fullPath = `${url}/${href}`;
+        // add the full path of all links found on the homepage
+        if (linksSeen.indexOf(fullPath) === -1) {
+          linksSeen.push(fullPath);
+        }
+      });
+      return linksSeen;
+    })
+    .catch((error) => {
+      console.log("First scrape failed");
+      printError(error);
     });
-    return (linksSeen);
-  })
-  .catch(function(error) {
-    console.log("First scrape failed");
-    errorFunc(error)
-  });
 }
 
 function getLinks(linksSeen) {
   const productPages = [];
   for (let i = 0; i < linksSeen.length; i++) {
-    if (linksSeen[i].indexOf("?id=") > 0 ) {
+    if (linksSeen[i].indexOf("?id=") > 0) {
       productPages.push(linksSeen[i]);
     } else {
       shirtsToScrape.push(linksSeen[i]);
     }
-    return { productPages: productPages, shirtsToScrape: shirtsToScrape };
+    return { productPages, shirtsToScrape };
   }
 }
 
@@ -92,31 +94,30 @@ function secondScrape(filterObj) {
   const shirtsForScrape = filterObj.shirtsToScrape;
   const promiseArray = [];
 
-  for(let j = 0; j < shirtsForScrape.length; j++){
+  for (let j = 0; j < shirtsForScrape.length; j++) {
     promiseArray.push(requestPromise(shirtsForScrape[j]));
     var promises = Promise.all(promiseArray);
   }
 
-  return(promises)
-    .then(function(promises){
+  return promises
+    .then((promises) => {
       for (let k = 0; k < promises.length; k++) {
         const $ = cheerio.load(promises[k]);
 
-        $("a[href*='shirt.php?id=']").each(function() {
+        $("a[href*='shirt.php?id=']").each(function () {
           const href = $(this).attr("href");
           const fullPath = url + "/" + href;
 
           if (productPages.indexOf(fullPath) === -1) {
             productPages.push(fullPath);
           }
-
         }); // Ends each loop
       } // End For
       return productPages;
     })
-    .catch(function(error){
+    .catch((error) => {
       console.log("Second scrape failed");
-      errorFunc(error);
+      printError(error);
     });
 }
 
@@ -124,18 +125,18 @@ function thirdScrape(productPages) {
   const promiseArray = [];
 
   for (var l = 0; l < productPages.length; l++) {
-      promiseArray.push(requestPromise(productPages[l]));
-      var promises = Promise.all(promiseArray);
+    promiseArray.push(requestPromise(productPages[l]));
+    var promises = Promise.all(promiseArray);
   }
 
-  return(promises)
-    .then(function(promises) {
+  return promises
+    .then((promises) => {
       for (var m = 0; m < promises.length; m++) {
         var $ = cheerio.load(promises[m]),
-            title = $("title").text(),
-            price = $(".price").text(),
-            img = $(".shirt-picture img").attr("src"),
-            shirts = {}; // Create empty JSON object with shirt data
+          title = $("title").text(),
+          price = $(".price").text(),
+          img = $(".shirt-picture img").attr("src"),
+          shirts = {}; // Create empty JSON object with shirt data
         shirts.Title = title;
         shirts.Price = ` ${price}`;
         shirts.ImageURL = ` ${url + img}`; // Log full path URL
@@ -145,29 +146,29 @@ function thirdScrape(productPages) {
       }
       return totalShirts;
     })
-    .catch(function(error){
-      Console.log("Third scrape failed")
-      errorFunc(error)
+    .catch((error) => {
+      Console.log("Third scrape failed");
+      printError(error);
     });
 }
 
-function writeToFile(shirtsData){
+function writeToFile(shirtsData) {
   // Create data folder if it doesn't exist
   if (!fs.existsSync("./data")) {
     fs.mkdirSync("./data");
   }
   // Use json-2-csv module to convert JSON
-  converter.json2csv(shirtsData, function(error, csv) {
+  converter.json2csv(shirtsData, (error, csv) => {
     if (error) {
       console.log("Error at the file writing step");
-      errorFunc(error);
+      printError(error);
     } else {
-      console.log("Writing to csv file.");
+      console.log(shirtsData);
       // create filename by appending .csv to the current date
       var newcsvname = "./data/" + scraperDate() + ".csv";
-      fs.writeFile(newcsvname, csv, function(error) {
+      fs.writeFile(newcsvname, csv, (error) => {
         if (error) {
-          errorFunc(error);
+          printError(error);
         }
       });
     }
@@ -178,8 +179,8 @@ function scraperDate() {
   // get new date object
   const d = new Date();
   const year = d.getFullYear();
-  const month = `0${(d.getMonth() + 1)}`;
+  const month = `0${d.getMonth() + 1}`;
   const day = d.getDate();
 
-  return [year, month, day].join('-');
+  return [year, month, day].join("-");
 } // ends scraperDate
